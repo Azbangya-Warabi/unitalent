b20840db73065e4bc4f72bb823685d919fcd2917
wonsik <whdnjstlr321@naver.com>
2021-06-05 03:17:20 +0900

    Initial commit

---
+++ src/main/java/kr/co/unitalent/UnitalentApplication.java
@@ -0,0 +1,13 @@
+package kr.co.unitalent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitalentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitalentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/unitalent/UnitalentApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.unitalent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitalentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
